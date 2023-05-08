812c9c47639e80775b1a027930f5c4ab28660964
Eugen Sadovnikov <sadeugen@gmail.com>
2023-05-09 01:58:49 +0300

    feat: implement task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("node:path");
+
+fs.readdir (
+  path.join (__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      fs.stat(
+        path.join(__dirname, `secret-folder/${file.name}`),
+        (err, stats) => {
+          if (!stats.isDirectory()) {
+            console.log(path.parse(file.name).name + " - " + path.parse(file.name).ext + " - " + stats.size + " bytes");
+          }
+        }
+      );
+    }
+  }
+);
