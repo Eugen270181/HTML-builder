a4638605855ffb21834312387ea7879b72e94e64
Eugen Sadovnikov <sadeugen@gmail.com>
2023-05-09 02:58:29 +0300

    feat: implement task 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("node:path");
+const fs = require('fs');
+const path = require('node:path');
 
 fs.readdir (
   path.join (__dirname, 'secret-folder'),
@@ -8,7 +8,7 @@ fs.readdir (
     if (err) throw err;
     for (let file of files) {
       fs.stat(
-        path.join(__dirname, `secret-folder/${file.name}`),
+        path.join(__dirname, 'secret-folder', file.name),
         (err, stats) => {
           if (!stats.isDirectory()) {
             console.log(path.parse(file.name).name + " - " + path.parse(file.name).ext + " - " + stats.size + " bytes");
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
+async function copyDir() {
+ await fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true } );
+ await fs.mkdir(path.join(__dirname, 'files-copy'));
+ 
+ const files = await fs.readdir(path.join(__dirname, 'files'));
+ 
+ for ( let file of files) {
+  await fs.copyFile( path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy',file));
+ }
+}
+
+copyDir();
